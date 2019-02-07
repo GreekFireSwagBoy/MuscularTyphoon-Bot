adee9cdf647b2a5f3f353c3fe5a5b069a53fb8db
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-02-06 21:05:06 -0500

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,11 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
+    .setTitle("Bot Information")
+    .setDescription("Created By: GreekFireSwagBoy")
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
+    .addField("Created On", bot.user.createdAt)
+
+
 
     message.channel.send(botembed);
 }
