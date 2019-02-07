96a5c1e6a6f1a61ea599a9beec829e2b5a7199c0
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-02-06 20:51:48 -0500

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,7 +8,6 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Created On", bot.user.createdAt);
-    .addField("Created By: GreekFireSwagBoy");
 
     message.channel.send(botembed);
 }
