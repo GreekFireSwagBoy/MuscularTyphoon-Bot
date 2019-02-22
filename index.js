460503e92fbf138a8bd4093bc6f79f189e6768ee
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-02-22 14:01:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("Zombsroyale.io", {type: "PLAYING"});
+  bot.user.setActivity("MODERATION ON DISCORD", {type: "PLAYING"});
 
 });
 
