3872f32f3f5ea7bbe71d7e8dd6fa327783abb6ca
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-02-28 14:26:21 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, mate looks like you do not have permissions to do that!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked, perhaps because of their role!");
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
