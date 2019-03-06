79792058ce488aed25f33d3b621344bc05d049b9
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-03-06 15:11:50 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "incidents");
+    let incidentchannel = message.guild.channels.find(`name`, "logs");
     if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
 
     message.guild.member(bUser).ban(bReason);
