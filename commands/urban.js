874c7022efa96f97b33e397aef50c79cea5f0280
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-03-06 21:15:07 -0500

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -1,23 +1,22 @@
-const Discord = module.require("discord.js");
-const urban = module.exports.require("urban");
+const Discord = require("discord.js");
+const urban = require("urban");
 
 module.exports.run = async (bot, message, args) => {
-if(args.length < 1) return message.channel.send("Please enter something.");
-  Let str = args.join(" ");
+    if(args.length < 1) return message.channel.send("Please enter a word.");
+    let str = args.join(" ");
 
-  urban(str).first(json => {
-      if (!json) return message.channel.send("No results found.");
-      console.log(json);
+    urban(str).first(json => {
+        if(!json) return message.channel.send("No results found.");
 
-      Let embed = new Discord.RichEmbed()
-          .setTitle(json.word)
-          .setDescription(json.definition)
-          .addField("Upvotes", json.thumbs_up, true)
-          .addField("Downvotes", json.thumbs_down, true);
-          .setFooter(`Written by ${json.author}`")
+        let embed = new Discord.RichEmbed()
+        .setTitle(json.word)
+        .setDescription(json.definition)
+        .addField("Upvotes 👍", json.thumbs_up, true)
+        .addField("Downvotes 👎", json.thumbs_down, true)
+        .setFooter(`Written by ${json.author}`);
 
-      message.channel.send({embed});
-  });
+    message.channel.send({embed});
+    });
 }
 
 module.exports.help = {
