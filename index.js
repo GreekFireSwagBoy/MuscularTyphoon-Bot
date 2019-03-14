14f1bf8f297976c85e68175ffa660a7b51af5d63
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-03-14 12:18:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("Ravenna Correctional Center Discord!", {type: "WATCHING"});
+  bot.user.setActivity("THINGS!!", {type: "WATCHING"});
 
 });
 
