3d07fb747bd0c9c5e5eac304b3fa3318f361f581
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-03-15 15:44:56 -0400

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let jsicon = "https://discord.js.org/static/logo-square.png"
+
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You do not have administrator!");
+let announcemessage = args.join(" ");
+message.delete().catch();
+
+let announceEmbed = new Discord.RichEmbed()
+.setTitle(`Important Announcement from ${message.author.username}`)
+.setDescription(announcemessage)
+.setColor("RANDOM")
+.addField("Author", `${message.author.username}#${message.author.discriminator}`)
+.setTimestamp()
+.setFooter("MuscularTyphoon Bot", jsicon);
+
+message.channel.send(`@everyone`, announceEmbed);
+message.author.send("I have successfully announced what you requested me to do.")
+}
+module.exports.help = {
+  name: "announce"
+}
