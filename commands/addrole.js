d6bc504c966e12012ee26a8d69e25e20b5f86289
GreekFireSwagBoy <40745022+GreekFireSwagBoy@users.noreply.github.com>
2019-03-20 15:15:56 -0400

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @greek Moderator+
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("NOT AUTHORIZED BY THE GOD MUSCULARTYPHOON MY MASTER.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("In-sufficent permissions to do this action.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user.");
+  if(!rMember) return message.channel.send("Couldn't find that user.");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
+  if(!role) return message.channel.send("Specify a role!");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Couldn't find that role.");
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  if(rMember.roles.has(gRole.id)) return message.channel.send("They already have that role.");
   await(rMember.addRole(gRole.id));
 
   try{
